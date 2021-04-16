3346e0a4723407a75f916ed58167b5402b3ab7b0
Soresa-Hailu <craft.soresa@gmail.com>
2021-04-15 22:10:34 -0400

    commit our first source code

---
+++ CalculatorProject/src/com/classofmarch2/PracticeClass.java
@@ -0,0 +1,12 @@
+package com.classofmarch2;
+
+public class PracticeClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello, THis is Biruk");
+		
+	}
+
+}
