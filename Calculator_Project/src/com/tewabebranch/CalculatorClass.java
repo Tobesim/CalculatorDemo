c9c3a4091f7d1fc3c43fd0fbab6adc3b03948db7
Tobesim <simreteaab@gmail.com>
2021-04-19 16:02:07 -0400

    I commit my first code.

---
+++ Calculator_Project/src/com/tewabebranch/CalculatorClass.java
@@ -0,0 +1,14 @@
+package com.tewabebranch;
+
+public class CalculatorClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Hello Soresa, Look Look, Tewabe is good in addition.");
+       int first = 25;
+    		  int second = 75;
+    		  int sum = first + second;
+    		  System.out.println("The sum is = " + sum);
+	}
+
+}
